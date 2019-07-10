1e2b74e9ce419d478105664879a0d01b236a47c2
Margot Konig <margot.konig@blackrock.com>
2019-07-10 17:27:02 +0000

    added user.rb model and signup, login, failure views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,70 @@
+# require "./config/environment"
+# require "./app/models/user"
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
   
+  # configure do
+  #   set :views, "app/views"
+  #   enable :sessions
+  #   set :session_secret, "password_security"
+  # end
+  
   get '/' do
     erb :home
   end
+  
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
 end
