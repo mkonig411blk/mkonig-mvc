d5d73739a5ea3503922d49889c1fa6539da6da6f
mkonig411blk <margot.konig@blackrock.com>
2019-07-10 15:23:24 -0500

    updated config & environment files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,13 @@
-# require "./config/environment"
-# require "./app/models/user"
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
-  
-  # configure do
-  #   set :views, "app/views"
-  #   enable :sessions
-  #   set :session_secret, "password_security"
-  # end
-  
+
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
   get '/' do
     erb :home
   end
-  
+
 end
