58b70614ccf4fe90d9bf64b080fb7ad82bcef204
Margot Konig <margot.konig@blackrock.com>
2019-07-10 17:46:57 +0000

    added users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController 
+  
+ get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
+end
